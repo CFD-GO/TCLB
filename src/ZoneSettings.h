19cf9d7f057236c1b5fd7032efb7de9d5bb44922
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2014-07-01 12:15:12 +0200

    Some changes to ZoneSettings and Control features

--- src/ZoneSettings.h
+++ src/ZoneSettings.h
@@ -8,38 +8,34 @@
 
 class ZoneSettings {
   int len;
-  real_t * gpuValues;
-  real_t * gpuControl;
   real_t ** cpuTab;
   real_t * cpuConst;
-  real_t * cpuValues;
-  real_t * cpuControl;
 
 public:
+  real_t ** cpuValues;
   real_t ** gpuTab;
   real_t * gpuConst;
     
   inline ZoneSettings() {
+    int len = 1;
+    cpuValues = (real_t**) malloc(sizeof(real_t*) * ZONE_MAX * ZONESETTINGS);
     cpuTab = (real_t**) malloc(sizeof(real_t*) * ZONE_MAX * ZONESETTINGS);
     for (int i=0; i<ZONE_MAX * ZONESETTINGS; i++) {
+      cpuValues[i] = NULL;
       cpuTab[i] = NULL;
+      printf("Constructor: %p %p\n", cpuValues[i], cpuTab[i]);
     }
     CudaMalloc(&gpuTab, sizeof(real_t*) * ZONE_MAX * ZONESETTINGS);
     cpuConst = (real_t*) malloc(sizeof(real_t) * ZONE_MAX * ZONESETTINGS);
     for (int i=0; i<ZONE_MAX * ZONESETTINGS; i++) {
-      cpuConst[i] = 1.01;
-    }
-    for (int i=0; i<ZONE_MAX; i++) {
-      for (int j=0; j<ZONESETTINGS; j++) {
-        cpuConst[j+ZONESETTINGS*i] = 0.01 * (i+1);
-      }
+      cpuConst[i] = 0.0;
     }
     CudaMalloc(&gpuTab,   sizeof(real_t*) * ZONE_MAX * ZONESETTINGS);
     CudaMalloc(&gpuConst, sizeof(real_t)  * ZONE_MAX * ZONESETTINGS);
     CopyToGPU();
   }
 
-  inline void set(int s, int z, real_t val) {
+  inline void set(int s, int z, double val) {
     assert(s >=  0);
     assert(s <   ZONESETTINGS);
     assert(z >= -1);
@@ -54,17 +50,60 @@ public:
     CopyToGPU();  
   }
   
+  inline void setLen(int nlen) {
+    for (int i=0; i<ZONE_MAX * ZONESETTINGS; i++) {
+      if (cpuValues[i] != NULL) free(cpuValues[i]);
+      if (cpuTab[i] != NULL) CudaFree(cpuTab[i]);
+    }
+    len = nlen;
+    CopyToGPU();  
+  }    
+
+  inline void Alloc(int i) {
+    assert(cpuValues[i] == NULL);
+    cpuValues[i] = (real_t*) malloc(sizeof(real_t) * len);
+    CudaMalloc(&cpuTab[i], sizeof(real_t) * len);
+    printf("Alloc: %p %p len:%d\n", cpuValues[i], cpuTab[i], len);
+  }    
+
+  inline void set(int s, int z, std::vector<double> val) {
+    assert(s >=  0);
+    assert(s <   ZONESETTINGS);
+    assert(z >= 0);
+    assert(z <   ZONE_MAX);
+    assert(val.size() == len);
+    int i = s+ZONESETTINGS*z;
+    Alloc(i);
+    for (int j=0; j<len; j++) {
+      cpuValues[i][j] = val[j];
+    }
+    CopyToGPU();  
+  }
+  
+
+  
   inline void CopyToGPU () {
     CudaMemcpy(gpuTab,   cpuTab,   sizeof(real_t*) * ZONE_MAX * ZONESETTINGS, cudaMemcpyHostToDevice);
     CudaMemcpy(gpuConst, cpuConst, sizeof(real_t)  * ZONE_MAX * ZONESETTINGS, cudaMemcpyHostToDevice);
+    for (int i=0; i<ZONE_MAX * ZONESETTINGS; i++) if (cpuValues[i] != NULL) {
+      printf("CopyToGPU: %p %p len:%d\n", cpuValues[i], cpuTab[i], len);
+      assert(cpuTab[i] != NULL);
+      CudaMemcpy(cpuTab[i],   cpuValues[i],  sizeof(real_t) * len, cudaMemcpyHostToDevice);
+    }
   }
 
   inline ~ZoneSettings() {
+    for (int i=0; i<ZONE_MAX * ZONESETTINGS; i++) {
+      if (cpuValues[i] != NULL) free(cpuValues[i]);
+      if (cpuTab[i] != NULL) CudaFree(cpuTab[i]);
+    }
     free(cpuTab);
-    CudaFree(&gpuTab);
+    CudaFree(gpuTab);
     free(cpuConst);
-    CudaFree(&gpuConst);
+    CudaFree(gpuConst);
   }
+  
+  inline int getLen() { return len; }
 };
 
 #endif
