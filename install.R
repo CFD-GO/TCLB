913eddc96bf528edebbfe7f4c2fe2442f3248005
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2015-01-14 13:57:56 +0100

    Adding install.R which installs all (I hope) needed R packages

---
+++ install.R
@@ -0,0 +1,6 @@
+#!/usr/bin/env Rscript
+options(repos="http://cran.rstudio.com")
+install.packages("devtools")
+devtools::install_github("llaniewski/rtemplate")
+devtools::install_github("llaniewski/gvector")
+devtools::install_github("llaniewski/polyAlgebra")
