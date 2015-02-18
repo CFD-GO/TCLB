a113d25820dfe20add3a72cca248cde633c1274c
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2015-02-18 17:27:15 +0100

    Adding new install script. will delete the old later on

---
+++ install.sh
@@ -0,0 +1,15 @@
+mkdir tmp
+pushd tmp
+rm *
+
+function github_install {
+	name=$(echo $1 | sed "s/\//./g")-$2
+	echo Downloading $name ...
+	wget https://github.com/$1/archive/$2.tar.gz -O $name.tar.gz >$name.wget.log 2>&1 || (echo failed; exit -1);
+	echo Installing $name ...
+	R CMD INSTALL $name.tar.gz >$name.install.log 2>&1|| (echo failed; exit -1);
+}
+
+github_install llaniewski/rtemplate v1.0
+github_install llaniewski/gvector v1.0
+github_install llaniewski/polyAlgebra v1.0
