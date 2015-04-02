3663e51ec9f3c5ac28f073be67f61bb467bbeb3a
Michal Dzikowski <mdzikowski@meil.pw.edu.pl>
2015-04-02 18:59:55 +0200

    in agreement with python, BC for PSI need to be enchanced

--- src/d2q9_npe_guo/Dynamics.R
+++ src/d2q9_npe_guo/Dynamics.R
@@ -39,32 +39,34 @@ AddDensity( name="h_1[6]", dx=-1, dy= 1, group="h_1")
 AddDensity( name="h_1[7]", dx=-1, dy=-1, group="h_1")
 AddDensity( name="h_1[8]", dx= 1, dy=-1, group="h_1")
 
-AddDensity(name="subiter", dx=0, dy=0)
+#AddDensity(name="subiter", dx=0, dy=0)
 
-AddDensity(name="grad_psi[0]", dx=0, dy=0)
-AddDensity(name="grad_psi[1]", dx=0, dy=0)
+#AddDensity(name="grad_psi[0]", dx=0, dy=0)
+#AddDensity(name="grad_psi[1]", dx=0, dy=0)
 
-AddDensity(name="psi",dx=0, dy=0)
+#AddDensity(name="psi",dx=0, dy=0)
 
 
-AddQuantity(name="u", vector=T)
+AddQuantity(name="F", vector=T)
+AddQuantity(name="U", vector=T)
+AddQuantity(name="Rho")
 AddQuantity(name="n0")
 AddQuantity(name="n1")
-AddQuantity(name="Subiter")
+#AddQuantity(name="Subiter")
 AddQuantity(name="Psi")
 AddQuantity(name="GradPsi", vector=T)
 
 
-AddStage("BaseIteration", "Run", save=Fields$group!="nonono", load=DensityAll$group!="nonono")
+#AddStage("BaseIteration", "Run", save=Fields$group!="nonono", load=DensityAll$group!="nonono")
 
-AddStage("CalcPsi", save="psi",load=DensityAll$group == "g")
+#AddStage("CalcPsi", save="psi",load=DensityAll$group == "g")
 
-AddStage("CalcSubiter", save="subiter",load="subiter")
+#AddStage("CalcSubiter", save="subiter",load="subiter")
 
 
 #AddStage("BaseInit", "Init", save=Fields$group == "g", load=DensityAll$group == "g")
 
-AddAction("Iteration", c("BaseIteration","CalcPsi", "CalcSubiter"))
+#AddAction("Iteration", c("BaseIteration","CalcPsi", "CalcSubiter"))
 #AddAction("Iteration", c("CalcSubiter"))
 
 #AddAction("Init", c("BaseInit"))
@@ -77,15 +79,15 @@ AddQuantity(name="rho_e",unit="kg/m3")
 AddSetting(name="tau_psi", comment='tau_psi')
 
 AddSetting(name="n_inf", comment='')
-AddSetting(name="z", comment='')
 AddSetting(name="el", comment='')
 AddSetting(name="kb", comment='')
 AddSetting(name="T", comment='')
 AddSetting(name="epsilon", comment='')
 AddSetting(name="dt", comment='')
 AddSetting(name="psi0", default=1., comment='')
-AddSetting(name="z0", default=1., comment='')
-AddSetting(name="z1", default=-1., comment='')
+AddSetting(name="ez", default=1., comment='')
+AddSetting(name="Ex", default=0, comment='')
+AddSetting(name="omega", default=1., comment='')
 
 
 
