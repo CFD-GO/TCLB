030c0a33f3f3d9c6420087bf7607a79171942fea
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2015-05-18 10:53:40 +0200

    Added entropic stabilizer and LES to experimental d2q9_new

--- src/d2q9_new/Dynamics.R
+++ src/d2q9_new/Dynamics.R
@@ -23,6 +23,9 @@ AddDensity( name="f[8]", dx= 1, dy=-1, group="f")
 AddQuantity(name="Rho",unit="kg/m3")
 AddQuantity(name="U",unit="m/s",vector=T)
 
+AddQuantity(name="A",unit="1",vector=T)
+
+
 # Settings - table of settings (constants) that are taken from a .xml file
 #  name - name of the constant variable
 #  comment - additional comment
@@ -32,6 +35,13 @@ AddSetting(name="omega", comment='one over relaxation time')
 AddSetting(name="nu", omega='1.0/(3*nu + 0.5)', default=1.6666666, comment='viscosity')
 AddSetting(name="Velocity", default=0, comment='inlet/outlet/init velocity', zonal=T)
 AddSetting(name="Pressure", default=0, comment='inlet/outlet/init density', zonal=T)
+AddSetting(name="Smag", comment='Smagorinsky constant')
+
+AddSetting(name="SL_U", comment='Shear Layer velocity')
+AddSetting(name="SL_lambda", comment='Shear Layer lambda')
+AddSetting(name="SL_delta", comment='Shear Layer disturbance')
+AddSetting(name="SL_L", comment='Shear Layer length scale')
+
 
 # Globals - table of global integrals that can be monitored and optimized
 
@@ -39,3 +49,5 @@ AddGlobal(name="PressureLoss", comment='pressure loss', unit="1mPa")
 AddGlobal(name="OutletFlux", comment='pressure loss', unit="1m2/s")
 AddGlobal(name="InletFlux", comment='pressure loss', unit="1m2/s")
 
+AddNodeType("Smagorinsky", "LES")
+AddNodeType("Stab", "ENTROPIC")
