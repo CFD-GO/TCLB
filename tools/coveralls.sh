b4a4f9ad532433aae36fa9b4a19568be4d9d5604
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2015-08-18 16:38:01 +0200

    coverall.sh exits without error if there are no file to analyse

--- tools/coveralls.sh
+++ tools/coveralls.sh
@@ -1,3 +1,10 @@
+F=$(find -name "*.gcda")
+if test -z "$F"
+then
+	echo coveralls: No GCDA files
+	echo Exiting without error
+	exit 0
+fi
 lcov --compat-libtool --directory . --capture --output-file coverage.info
 lcov --list coverage.info
 echo coveralls-lcov coverage.info
