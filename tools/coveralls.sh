33a59b063f078b37feabe551d76e63a9bd292b7f
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2015-08-19 13:42:52 +0200

    First coveralls

--- tools/coveralls.sh
+++ tools/coveralls.sh
@@ -6,5 +6,6 @@ then
 	exit 0
 fi
 lcov --compat-libtool --directory . --capture --output-file coverage.info
+lcov --remove coverage.info "/usr*" -o coverage.info
 lcov --list coverage.info
-echo coveralls-lcov coverage.info
+coveralls-lcov coverage.info
