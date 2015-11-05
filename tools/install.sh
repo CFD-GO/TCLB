6211a5f73bebb05a57dd42ee513833d6321eb771
Tomasz Gajek <TGajek@users.noreply.github.com>
2015-11-05 11:23:09 +0100

    Added "-y" in install of r in install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -100,7 +100,7 @@ r)
 	try "Adding repository" add-apt-repository "deb ${CRAN}/bin/linux/ubuntu $DIST/"
 	try "Adding repository key" apt-key adv --keyserver hkp://keyserver.ubuntu.com:80/ --recv-keys E084DAB9
 	try "Updating APT" apt-get update -qq
-	try "Installing R base" apt-get install --no-install-recommends r-base-dev r-recommended qpdf
+	try "Installing R base" apt-get install -y --no-install-recommends r-base-dev r-recommended qpdf
 	try "Changing access to R lib paths" chmod 2777 /usr/local/lib/R /usr/local/lib/R/site-library
 	;;
 rdep)
