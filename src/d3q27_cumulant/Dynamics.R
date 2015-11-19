c2a1ec95a87873c245997b98ed90729bb2642e21
dmshko <shkodm@gmail.com>
2015-11-19 11:50:07 +0100

    changes to galilean correction

--- src/d3q27_cumulant/Dynamics.R
+++ src/d3q27_cumulant/Dynamics.R
@@ -1,8 +1,10 @@
 
-U = expand.grid(-1:1,-1:1,-1:1)
+x = c(0,1,-1);
+P = expand.grid(x=0:2,y=0:2,z=0:2)
+U = expand.grid(x,x,x)
 
 AddDensity(
-	name = paste("f",1:27-1,sep=""),
+	name = paste("f",P$x,P$y,P$z,sep=""),
 	dx   = U[,1],
 	dy   = U[,2],
 	dz   = U[,3],
@@ -13,13 +15,13 @@ AddDensity(
 AddQuantity( name="P",unit="Pa")
 AddQuantity( name="U",unit="m/s",vector=T)
 
-AddSetting(name="omega", comment='One over relaxation time')
-AddSetting(name="nu", omega='1.0/(3*nu + 0.5)', default=1.6666666, comment='Viscosity')
+AddSetting(name="nu", default=1.6666666, comment='Viscosity',zonal=TRUE)
 AddSetting(name="Velocity", default="0m/s", comment='Inlet velocity', zonal=TRUE)
 AddSetting(name="Pressure", default="0Pa", comment='Inlet pressure', zonal=TRUE)
 AddSetting(name="Smag", comment='Smagorinsky constant')
 AddSetting(name="Turbulence", comment='Turbulence intensity', zonal=TRUE)
 
+AddSetting(name="GalileanCorrection",default=0.,comment='Galilean correction term')
 AddSetting(name="ForceX", comment='Force force X')
 AddSetting(name="ForceY", comment='Force force Y')
 AddSetting(name="ForceZ", comment='Force force Z')
