77c1fa38a1af3a8e65ef8f4a24382c40f6cfaa5e
dmshko <dmshko@gmail.com>
2016-02-11 17:33:50 +0100

    Adding developing version of the Sampler class

--- src/Sampler.h
+++ src/Sampler.h
@@ -13,26 +13,28 @@ class Sampler {
        //        	double * gpu_buffer; 
        	public:
 		Sampler();
+		lbRegion position;
 		double *gpu_buffer;
                	Location location;
                	name_set *quant;
                	int size;
+		int startIter;
                	int initCSV(const char* name);
                	int writeHistory(int curr_iter);
-               	int Allocate(name_set* quantities,int total_iter);
+               	int Allocate(name_set* quantities,int total_iter,int noidea);
                	const char *filename;
-
+		int Finish();
        	};
-inline int csvWriteElement(FILE * f, float tmp) { return fprintf(f, "%g\n" , tmp); }
-inline int csvWriteElement(FILE * f, double tmp) { return fprintf(f, "%lg\n" , tmp); }
-inline int csvWriteElement(FILE * f, float3 tmp) { return fprintf(f, "%g\t,%g\t,%g\n" , tmp.x, tmp.y, tmp.z); }
-inline int csvWriteElement(FILE * f, double3 tmp) { return fprintf(f, "%lg\t%lg\t%lg\n" , tmp.x, tmp.y, tmp.z); }
+inline int csvWriteElement(FILE * f, float tmp) { return fprintf(f, "%g\t" , tmp); }
+inline int csvWriteElement(FILE * f, double tmp) { return fprintf(f, "%lg\t" , tmp); }
+inline int csvWriteElement(FILE * f, float3 tmp) { return fprintf(f, "%g\t,%g\t,%g\t" , tmp.x, tmp.y, tmp.z); }
+inline int csvWriteElement(FILE * f, double3 tmp) { return fprintf(f, "%lg\t%lg\t%lg\t" , tmp.x, tmp.y, tmp.z); }
 inline int csvWriteElement(FILE * f, vector_t tmp) {
        	csvWriteElement(f, tmp.x);
        	fprintf(f,"\t");
        	csvWriteElement(f, tmp.y);
        	fprintf(f,"\t");
        	return csvWriteElement(f, tmp.z);
-       	fprintf(f,"\n");
+       	fprintf(f,"\t");
 }
 
