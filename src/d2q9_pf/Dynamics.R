c0296d5330ceba50d06bb9dd18f34989b0b0d0e0
mdzik <dziki01@gmail.com>
2016-03-31 14:02:49 +0200

    Working phasefield model

--- src/d2q9_pf/Dynamics.R
+++ src/d2q9_pf/Dynamics.R
@@ -13,7 +13,17 @@ AddDensity( name="f[6]", dx=-1, dy= 1, group="f")
 AddDensity( name="f[7]", dx=-1, dy=-1, group="f")
 AddDensity( name="f[8]", dx= 1, dy=-1, group="f")
 
-AddField(name="f[1]", dx=1);
+
+AddDensity( name="h[0]", dx= 0, dy= 0, group="h")
+AddDensity( name="h[1]", dx= 1, dy= 0, group="h")
+AddDensity( name="h[2]", dx= 0, dy= 1, group="h")
+AddDensity( name="h[3]", dx=-1, dy= 0, group="h")
+AddDensity( name="h[4]", dx= 0, dy=-1, group="h")
+AddDensity( name="h[5]", dx= 1, dy= 1, group="h")
+AddDensity( name="h[6]", dx=-1, dy= 1, group="h")
+AddDensity( name="h[7]", dx=-1, dy=-1, group="h")
+AddDensity( name="h[8]", dx= 1, dy=-1, group="h")
+
 
 
 
@@ -27,7 +37,8 @@ AddField(name="f[1]", dx=1);
 AddQuantity(name="Rho",unit="kg/m3")
 AddQuantity(name="U",unit="m/s",vector=T)
 
-AddQuantity(name="A",unit="1",vector=T)
+AddQuantity(name="Normal",unit="1/m",vector=T)
+AddQuantity(name="PhaseField",unit="1")
 
 
 # Settings - table of settings (constants) that are taken from a .xml file
@@ -36,9 +47,14 @@ AddQuantity(name="A",unit="1",vector=T)
 # You can state that another setting is 'derived' from this one stating for example: omega='1.0/(3*nu + 0.5)'
 
 AddSetting(name="omega", comment='one over relaxation time')
-AddSetting(name="nu", omega='1.0/(3*nu + 0.5)', default=1.6666666, comment='viscosity')
+AddSetting(name="nu", omega='1.0/(3*nu + 0.5)', default=0.16666666, comment='viscosity')
 AddSetting(name="Velocity", default=0, comment='inlet/outlet/init velocity', zonal=T)
 AddSetting(name="Pressure", default=0, comment='inlet/outlet/init density', zonal=T)
+AddSetting(name="W", default=1, comment='Anty-diffusivity coeff')
+AddSetting(name="M", default=1, comment='Mobility')
+AddSetting(name="PhaseField", default=1, comment='Phase Field marker scalar', zonal=T)
+AddSetting(name="GravitationX", default=0)
+AddSetting(name="GravitationY", default=0)
 
 # Globals - table of global integrals that can be monitored and optimized
 
