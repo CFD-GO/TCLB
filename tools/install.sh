2de70499ffa913fdac149e950a96434ae07efdc1
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2016-12-29 22:57:33 +0100

    Adding rinside to travis installation

--- tools/install.sh
+++ tools/install.sh
@@ -122,6 +122,9 @@ rpython)
 	normal_install rjson
 	normal_install rPython
 	;;
+rinside)
+	normal_install RInside
+	;;
 cuda)
 	test -z "$1" && error Version number needed for cuda install
 	CUDA=$1
