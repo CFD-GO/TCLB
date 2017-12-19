8cc9daf02728bbc37e2cf165db2e6f83fa6ca223
TravisMitchell <uqtmitc3@goliath.labs.eait.uq.edu.au>
2017-12-19 14:22:35 +1000

    + Fix for velocity/pressure B.C, removal of symmetry conditions (now autosym can be used), addition of description

--- models/multiphase/d2q9_pf_velocity/conf.mk
+++ models/multiphase/d2q9_pf_velocity/conf.mk
@@ -1,6 +1,6 @@
 ADJOINT=0
 TEST=FALSE
-OPT="GF+RT"
+OPT="(GF+RT+Outflow)*autosym"
 # GF: Guo Forcing;
 #	This is using a higher order Forcing scheme
 #	from the work of Guo et al. (2002) for the hydrodynamics
@@ -8,3 +8,9 @@ OPT="GF+RT"
 #	This is using the Temporal term included in the 
 #	phase field equilibrium distribution function by
 #	Ren et al. (2016)
+# Outflow: 
+# 	This is used for outflow boundaries, it is made as an
+# 	option as it requires additional fields for calculations
+# 	so results in a slower code.
+# autosym:
+# 	Allows symmetry node type flags introduced in v6.2
