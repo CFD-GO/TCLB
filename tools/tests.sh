0949093b2ce908395a375a7e129ca6f7a9053760
L Laniewski-Wollk <uqllanie@uq.edu.au>
2019-09-26 10:59:19 +1000

    Some cleanup and fixes to the test.sh script

--- tools/tests.sh
+++ tools/tests.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 function usage {
-	echo "tests.sh [-r n] [-v] MODEL [TESTS]"
-	exit -2
+	echo "tests.sh [-h] [-r n] [-v] MODEL [TESTS]"
+	if test "x$1" == "xhelp"
+	then
+		echo "         -h|--help        Help (this message)"
+		echo "         -r|--repeat n    Repeat test n times"
+		echo "         -v|--verbose     Verbose output"
+		echo "         MODEL   The name of the model to test"
+		echo "         TESTS   The list of test to run (optional)"
+	fi
 }
 
 function try {
 	comment="$1"
-	log=$(echo $comment | sed 's/ /./g').log
+	mkdir -p output/
+	log=output/$(echo $comment | sed 's/ /./g').log
 	shift
 	echo -n "$comment... "
 	if env time -f "%e" -o $log.time "$@" >$log 2>&1
@@ -29,29 +37,40 @@ function try {
 	return 0;
 }
 
-REPEAT=1
-
-if [[ "x$1" == "x-r"  ]];
-then
-    REPEAT=$2
-    if test $REPEAT -le 1;
-    then
-        REPEAT=2
-    fi
-    shift
-    shift
-fi
-
+REPEATS=1
 VERBOSE=false
 
-if [[ "x$1" == "x-v"  ]];
-then
-    VERBOSE=true
-    shift
-fi
+while true
+do
+	case "$1" in
+	-r|--repeat)
+		REPEATS="$2"
+		if ! test "$REPEATS" -gt 0
+		then
+			echo "Error: number of repeats have to be greater then 0"
+			exit -1
+		fi
+		shift
+		;;
+	-v|--verbose)
+		VERBOSE=true
+		;;
+	-h|--help)
+		usage help
+		exit 0;
+		;;
+	*)
+		break;
+	esac
+	shift
+done
 
-test -z "$1" && usage
 MODEL=$1
+if test -z "$MODEL"
+then
+	usage
+	exit -1;
+fi
 shift
 
 if ! test -f "CLB/$MODEL/main"
@@ -91,107 +110,99 @@ then
 fi
 
 
-function testModel {
-
 GLOBAL="OK"
 export PYTHONPATH="$PYTHONPATH:tools/python:tests/$MODEL"
 
-for t in $TESTS
-do
-	name="${t%.xml}"
-	t="$name.xml"
-	RESULT="FAILED"
-	
-	if test -f "tests/$MODEL/$t"
-	then
-		if try "Running \"$name\" test" CLB/$MODEL/main "tests/$MODEL/$t"
+function testModel {
+	for t in $TESTS
+	do
+		name="${t%.xml}"
+		t="$name.xml"
+		RESULT="FAILED"
+		
+		if test -f "tests/$MODEL/$t"
 		then
-			RESULT="OK"
-			RES=$(cd tests/$MODEL; find -name "${name}_*")
-			if ! test -z "$RES"
+			if try "Running \"$name\" test" CLB/$MODEL/main "tests/$MODEL/$t"
 			then
-				for r in $RES
-				do
-					g=tests/$MODEL/$r
-					echo -n " > Checking $r... "
-					EXT=${r##*.}
-					if test -f "$r" || [[ "x$EXT" == "xsha1" ]]
-					then
-						if ! test -f "$g"
+				RESULT="OK"
+				RES=$(cd tests/$MODEL; find -name "${name}_*")
+				if ! test -z "$RES"
+				then
+					for r in $RES
+					do
+						g=tests/$MODEL/$r
+						echo -n " > Checking $r... "
+						EXT=${r##*.}
+						if test -f "$r" || [[ "x$EXT" == "xsha1" ]]
 						then
-							echo "$g not found - this should not happen!"
-							exit -123
-						fi
-						R="WRONG"
-						COMMENT=""
-						case "$EXT" in
-						csv)
-							COMMENT="(csvdiff)"
-							tools/csvdiff -a "$r" -b "$g" -x 1e-10 -d Walltime >/dev/null && R="OK"
-							;;
-						sha1)
-							COMMENT="(SHA1 checksum)"
-							sha1sum -c "$g" >/dev/null 2>&1 && R="OK"
-							;;
-						*)
-							diff "$r" "$g" >/dev/null && R="OK"
-							;;
-						esac
-
-						if test "x$R" == "xOK"
-						then
-							echo "OK $COMMENT"
-						else
-							echo "Different $COMMENT"
-							if test "x$EXT" == "xsha1"
+							if ! test -f "$g"
 							then
-								cat $g
-								pat=$(cat $g | sed 's/.*[ ][ ]*//')
-								test -z "$pat" || sha1sum $pat
+								echo "$g not found - this should not happen!"
+								exit -123
 							fi
+							R="WRONG"
+							COMMENT=""
+							case "$EXT" in
+							csv)
+								COMMENT="(csvdiff)"
+								tools/csvdiff -a "$r" -b "$g" -x 1e-10 -d Walltime >/dev/null && R="OK"
+								;;
+							sha1)
+								COMMENT="(SHA1 checksum)"
+								sha1sum -c "$g" >/dev/null 2>&1 && R="OK"
+								;;
+							*)
+								diff "$r" "$g" >/dev/null && R="OK"
+								;;
+							esac
+
+							if test "x$R" == "xOK"
+							then
+								echo "OK $COMMENT"
+							else
+								echo "Different $COMMENT"
+								if test "x$EXT" == "xsha1"
+								then
+									cat $g
+									pat=$(cat $g | sed 's/.*[ ][ ]*//')
+									test -z "$pat" || sha1sum $pat
+								fi
+								RESULT="WRONG"
+							fi
+						else
+							echo "Not found"
 							RESULT="WRONG"
 						fi
-					else
-						echo "Not found"
-						RESULT="WRONG"
-					fi
-				done
+					done
+				fi
 			fi
+		else
+			echo "$t: test not found"
+			RESULT="NOT FOUND"
 		fi
-	else
-		echo "$t: test not found"
-		RESULT="NOT FOUND"
-	fi
-	if ! test "x$RESULT" == "xOK"
-	then
-		echo " > Test \"$name\" returned: $RESULT"
-		GLOBAL="FAILED"
-	fi
-done
-
-
-if test $REPEAT -eq 1
-then
-    if test "x$GLOBAL" == "xOK"
-    then
-    	exit 0
-    else
-    	echo "Some tests failed"
-    	exit -1
-    fi
-fi
-
+		if ! test "x$RESULT" == "xOK"
+		then
+			echo " > Test \"$name\" returned: $RESULT"
+			GLOBAL="FAILED"
+		fi
+	done
 }
-echo "Tests are gonna be repeated $REPEAT times, output/ and Running*.log will be moved."
-sleep 5
-echo "Running..."
 
-while test $REPEAT -gt 0
+rm -r output-$MODEL-*/ 2>/dev/null || true
+
+REPEAT=1
+while test "$REPEAT" -le "$REPEATS"
 do
-    echo "$REPEAT to go..."
-    testModel
-    #BELOW IS NOT PERFORMED ON LAST/ONLY TEST, testModel ends with exit X
-    mv -v output output-$REPEAT
-    mv -v Running* output-$REPEAT
-    REPEAT=$(expr $REPEAT - 1)
+	test "$REPEATS" -gt "1" && echo "############ repeat: $REPEAT ################"
+	testModel
+	mv -v output/ "output-$MODEL-$REPEAT"
+	REPEAT=$(expr $REPEAT + 1)
 done
+
+if test "x$GLOBAL" == "xOK"
+then
+	exit 0
+else
+	echo "Some tests failed"
+	exit -1
+fi
