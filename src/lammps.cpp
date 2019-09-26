239966b8db86e9cf82b42f96f87b77a275a8c60c
L Laniewski-Wollk <uqllanie@uq.edu.au>
2019-09-26 18:44:23 +1000

    An empty integrator added as lammps

--- src/lammps.cpp
+++ src/lammps.cpp
@@ -11,7 +11,8 @@ int main(int argc, char *argv[])
    MPMD.Init(MPI_COMM_WORLD, "LAMMPS");
    MPMD.Identify();
 
-   rfi::RemoteForceInterface< rfi::ForceCalculator, rfi::RotParticle > RFI;
+   rfi::RemoteForceInterface< rfi::ForceIntegrator, rfi::RotParticle > RFI;
+   RFI.name = "LAMMPS";
    
    MPMDIntercomm inter = MPMD["TCLB"];
    if (!inter) {
@@ -22,6 +23,39 @@ int main(int argc, char *argv[])
    ret = RFI.Connect(MPMD.work,inter.work);
    if (ret) return ret;
    assert(RFI.Connected());
+
+   double di = 100.0 * MPMD.work_rank;
+   for (int i = 0; i < RFI.Workers(); i++) {
+     RFI.Size(i) = 3;
+   }
+   RFI.Alloc();
+   for (size_t i = 0; i < RFI.size(); i++) {
+     RFI.setData(i, RFI_DATA_R,      di+i+0.001);
+     RFI.setData(i, RFI_DATA_POS+0,  di+i+0.002);
+     RFI.setData(i, RFI_DATA_POS+1,  di+i+0.003);
+     RFI.setData(i, RFI_DATA_POS+2,  di+i+0.004);
+     RFI.setData(i, RFI_DATA_VEL+0,  di+i+0.005);
+     RFI.setData(i, RFI_DATA_VEL+1,  di+i+0.006);
+     RFI.setData(i, RFI_DATA_VEL+2,  di+i+0.007);
+     if (RFI.Rot()) {
+       RFI.setData(i, RFI_DATA_ANGVEL+0,  i+0.008);
+       RFI.setData(i, RFI_DATA_ANGVEL+1,  i+0.009);
+       RFI.setData(i, RFI_DATA_ANGVEL+2,  i+0.010);
+     }
+   }
+   
+   while ( RFI.Active() ) {
+       printf("LAMMPS: RFI.SendSizes()\n");
+       RFI.SendSizes();
+       printf("RFI size sent ...\n");
+       RFI.SendParticles();
+       printf("RFI particles send ...\n");
+       RFI.SendForces();
+       printf("RFI forces send ...\n");
+   }
+   
+   RFI.Close();
+
 
    RFI.Close();
    MPI_Finalize();
