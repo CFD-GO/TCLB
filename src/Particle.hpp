38421c86fd988565adf7bfd00a8290c5c38737f0
L Laniewski-Wollk <uqllanie@uq.edu.au>
2019-11-26 14:03:09 +1000

    Correcting the invalid read error

--- src/Particle.hpp
+++ src/Particle.hpp
@@ -92,7 +92,7 @@ struct ParticleS : ParticleI {
 		atomicSumWarp(&constContainer.particle_data[i*RFI_DATA_SIZE+RFI_DATA_MOMENT+1],moment.y);
 		atomicSumWarp(&constContainer.particle_data[i*RFI_DATA_SIZE+RFI_DATA_MOMENT+2],moment.z);*/
 		double val[6] = {force.x,force.y,force.z,moment.x,moment.y,moment.z};
-		atomicSumWarpArr(&constContainer.particle_data[i*RFI_DATA_SIZE+RFI_DATA_MOMENT],val,6);
+		atomicSumWarpArr(&constContainer.particle_data[i*RFI_DATA_SIZE+RFI_DATA_FORCE],val,6);
 #elif PARTICLE_SYNC == BLOCK_SYNC
 		atomicSum(&constContainer.particle_data[i*RFI_DATA_SIZE+RFI_DATA_FORCE+0],force.x);
 		atomicSum(&constContainer.particle_data[i*RFI_DATA_SIZE+RFI_DATA_FORCE+1],force.y);
