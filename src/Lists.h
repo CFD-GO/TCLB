e538a27801779fbde8a10d2e524c2925f0e15f81
L Laniewski-Wollk <uqllanie@uq.edu.au>
2020-01-03 14:41:38 +1000

    Moving to Lists for Settings in Lattice

--- src/Lists.h
+++ src/Lists.h
@@ -4,14 +4,23 @@
 
 #define LIST_INVALID -1
 
-class SomethingList {
+class ListBase {
+public: 
     virtual int IdxFromString (const std::string& str) const;
     virtual std::string StringFromIdx (int i) const;
     virtual const char* CStringFromIdx (int i) const;
     virtual int size () const;
 };
 
-class SettingsList : public SomethingList {
+class SettingsListBase : public ListBase {
+public:
+    virtual int DerivedFromIdx(int i) const = 0;
+};
+
+class SettingsList : public SettingsListBase {
+public:
+    virtual int DerivedFromIdx(int i) const;
+    virtual double DerivedValueFromIdx(int i, double val) const;
     virtual int IdxFromString (const std::string& str) const;
     virtual const char* CStringFromIdx (int i) const;
     virtual int size () const;
