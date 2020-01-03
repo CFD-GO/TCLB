e0c326baec8a7f1078c6ce1e3366a3a6371ba256
L Laniewski-Wollk <uqllanie@uq.edu.au>
2020-01-03 16:48:11 +1000

    Moving Settings to Lists in more places

--- src/Lists.h
+++ src/Lists.h
@@ -12,26 +12,34 @@ public:
     virtual int size () const;
 };
 
-class SettingsListBase : public ListBase {
+class UnitListBase : public ListBase {
+public:
+    virtual std::string UnitFromIdx(int i) const;
+};
+
+class SettingsListBase : public UnitListBase {
 public:
     virtual int DerivedFromIdx(int i) const = 0;
+    virtual double DerivedValueFromIdx(int i, double val) const = 0;
 };
 
 class SettingsList : public SettingsListBase {
 public:
     virtual int DerivedFromIdx(int i) const;
     virtual double DerivedValueFromIdx(int i, double val) const;
+    virtual std::string UnitFromIdx(int i) const;
     virtual int IdxFromString (const std::string& str) const;
     virtual const char* CStringFromIdx (int i) const;
     virtual int size () const;
 };
 
-class ZoneSettingsListBase : public ListBase {
+class ZoneSettingsListBase : public UnitListBase {
 public:
 };
 
 class ZoneSettingsList : public ZoneSettingsListBase {
 public:
+    virtual std::string UnitFromIdx(int i) const;
     virtual int IdxFromString (const std::string& str) const;
     virtual const char* CStringFromIdx (int i) const;
     virtual int size () const;
