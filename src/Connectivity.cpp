0b40c960875d993553131baaa83dee0bfc34fd69
trisbsamson <trisbsamson@gmail.com>
2020-08-19 23:40:43 +1000

    Now reads NodeType info from connectivity file.
    Notes:
    	- Currently masks everything except Wall with MRT for the collision type
    	- will need to fix up format once we generalise the pre-processor
    	- Have disabled the FlagOverwrite function only in the Geometry handler
    		as I still need some of the code called by Geometry for allocating the
    		Lattice (for now)

--- src/Connectivity.cpp
+++ src/Connectivity.cpp
@@ -29,12 +29,20 @@ int Connectivity::load(pugi::xml_node & node) {
         error("Connection file can't be opened\n");
         return -1;
     }
-    char buffer[80];
+    char buffer[20];
     // read header information
     ret = fscanf(cxnFile, "LATTICESIZE %d\n", &latticeSize);
     ret = fscanf(cxnFile, "DIMENSION %d\n", &d);
     ret = fscanf(cxnFile, "Q %d\n", &Q);
+    ret = fscanf(cxnFile, "MASK %s\n", buffer);
     ret = fscanf(cxnFile, "NODES\n");
+
+    ModelBase::NodeTypeFlags::const_iterator it = model->nodetypeflags.ByName(buffer);
+    if (it == model->nodetypeflags.end()) {
+        ERROR("Unknown flag (in xml): %s\n", buffer);
+        return -1;
+    }
+    big_flag_t defMask = it->flag;
 
     // allocate memory for connectivity / nodetype matrices
     connectivity = (size_t*) malloc(latticeSize * Q * sizeof(size_t));
@@ -53,6 +61,20 @@ int Connectivity::load(pugi::xml_node & node) {
         coords[i] = x;
         coords[i + latticeSize] = y;
         coords[i + 2*latticeSize] = z;
+
+        // find the flag corresponding to our nodeType
+        it = model->nodetypeflags.ByName(nodeType);
+        if (it == model->nodetypeflags.end()) {
+            ERROR("Unknown flag (in xml): %s\n", nodeType);
+            return -1;
+        }
+        // set it to that in the nodetype array
+        // if it's a wall, don't set it to mrt as well
+        // if it's anything else, & it with mrt.. will need to think of a more elegant way to handle this when I generalise the pre-processor
+        if(it->flag == (0x000a))
+            geom[i] = it->flag;
+        else
+            geom[i] = it->flag | defMask;
 
         // next scan in the connectivity - have to scan an unknown number of integers
         for(int q = 0; q < Q - 1; q++) {
