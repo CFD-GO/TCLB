2ddad39fcb6e5a905328dab9357517fa304458f4
trisbsamson <trisbsamson@gmail.com>
2021-01-04 21:17:05 +1000

    Modified internal lattice kernel to be a single templated function. Templated function now calls the corresponding RunKernel function defined in the specified Accessor struct.

--- src/templates/ContainerAccessors.cpp
+++ src/templates/ContainerAccessors.cpp
@@ -15,7 +15,25 @@ struct ArbitraryLatticeContainerAccessor {
 		return constArbitraryContainer;
 	}
 
-	// run functions go here
+	/// Main function called by the Kernel for cartesian lattice
+	/**
+	 iterates over all elements and runs them with RunElement function.
+	constContainer.dx/dy is to calculate only internal nodes
+	*/
+	template <class N> CudaDeviceFunction static void RunKernel() {
+		N now;
+
+		now.idx_.id = CudaThread.x + (CudaThread.y * CudaNumberOfThreads.x) + 
+						(CudaBlock.x * CudaNumberOfThreads.x * CudaNumberOfThreads.y) + 
+						(CudaBlock.y * CudaNumberOfThreads.x * CudaNumberOfThreads.y * constArbitraryContainer.bnx);
+		now.Pre();
+		if(now.idx_.id < constArbitraryContainer.latticeSize) {
+			now.RunElement();
+		} else {
+			now.OutOfDomain();
+		}
+		now.Glob();
+	}
 };
 
 // Accessor for CartesianLatticeContainer
@@ -28,8 +46,31 @@ struct CartesianLatticeContainerAccessor {
 		return constContainer;
 	}
 
-	// run functions go here
+	/// Main function called by the Kernel for arbitrary lattice
+	/**
+	 iterates over all elements and runs them with RunElement function.
+	constContainer.dx/dy is to calculate only internal nodes
+	*/
+	template <class N> CudaDeviceFunction static void RunKernel() {
+		N now;
+		now.idx_.x = CudaThread.x + CudaBlock.z*CudaNumberOfThreads.x + constContainer.dx;
+		now.idx_.y = CudaThread.y + CudaBlock.x*CudaNumberOfThreads.y + constContainer.dy;
+		now.idx_.z = CudaBlock.y + constContainer.dz;
 
+		#ifndef GRID3D
+			for (; now.idx_.x < constContainer.nx; now.idx_.x += CudaNumberOfThreads.x) {
+		#endif
+			now.Pre();
+			if (now.idx_.y < constContainer.fy) {
+				now.RunElement();
+			} else {
+				now.OutOfDomain();
+			}
+			now.Glob();
+		#ifndef GRID3D
+			}
+		#endif
+	}
 };
 
 #define CONTAINERACCESSORS 1
