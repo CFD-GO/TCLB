4e4a3a9f159a8dd80cb4c059a02ca1443cf2d8a1
TravisMitchell <travis.mitchell@uqconnect.edu.au>
2021-07-09 15:55:55 +1000

    + added checkpoint feature

--- src/Handlers/cbSaveCheckpoint.cpp
+++ src/Handlers/cbSaveCheckpoint.cpp
@@ -5,27 +5,26 @@ std::string cbSaveCheckpoint::xmlname = "SaveCheckpoint";
 int cbSaveCheckpoint::Init () {
 		Callback::Init();
 		/*
-			This component gets both the restart file (rf) name
-			and the save file name (fn) for the default case. 
-			If the user wants to print out checkpoints with time
-			increments, then we need to use solver to get iter
-			number.
+			Initialisation of handler checks for keep attribute 
+			inside of SaveCheckpoint. Keep attribute can be used
+			to save the last "x" number of checkpoints, or specify
+			"all" to keep all points. 
+			DEFAULT behaviour is to return only the most recent 
+			checkpoint.
 		*/
-		overwrite = true;
-		pugi::xml_attribute attr = node.attribute("overwrite");
-		if (attr) overwrite = attr.as_bool();
-
-		if (overwrite) {
-			char restartFile[2*STRING_LEN];
-			char filename[2*STRING_LEN];
-			solver->outGlobalFile("restart", ".xml", restartFile);
-			rf = restartFile;
-
-			solver->outGlobalFile("CheckPoint", "", filename);
-			fn = filename;
-			writeRestartFile();
+		pugi::xml_attribute attr = node.attribute("keep");
+		if (attr) {
+			if (std::string(attr.value()) == "all"){
+				// Look for the keyword all and use keep = 0 as flag to store all
+				keep = 0;
+			} else {
+				// If the attr value is not the string all, assume it is an int
+				keep = attr.as_int();
+			}
+		} else{
+			keep = 1;
 		}
-		
+
 		return 0;
 	}
 
@@ -34,24 +33,47 @@ int cbSaveCheckpoint::DoIt () {
 		Callback::DoIt();
 		/*
 			Here we saveSolution to a _x.pri file where x is the MPI rank.
-			Note that we write over the xml file for each checkpoint.
+			If keep == 0, then we save all solutions. Otherwise, we check
+			the size of the queue; less than keep then save file, else
+			delete the first set into the queue
 		*/
-		if (overwrite){
-			solver->lattice->saveSolution(fn.c_str());
-		} else {
-			fn = "CheckPoint";
-			char restartFile[2*STRING_LEN];
-			char filename[2*STRING_LEN];
-			solver->writeSAVECHECKPOINT(fn.c_str(), filename, restartFile);
-			fn = filename;
-			rf = restartFile;
-			writeRestartFile(); 
+		output("writing checkpoint");
+		char restartFile[2*STRING_LEN];
+		char filename[2*STRING_LEN];
+		std::string fileStr;
+		std::string restStr;
+
+		solver->outIterCollectiveFile("checkpoint", "", filename);
+		solver->outIterCollectiveFile("restart", ".xml", restartFile);
+		
+		fileStr = solver->lattice->saveSolution(filename);
+
+		myqueue.push( fileStr );
+		if (D_MPI_RANK == 0 ) {
+			writeRestartFile(filename, restartFile);
+			restStr = restartFile;
+			myqueue_rst.push( restStr );
+		}
+
+		if (keep != 0){
+			if (myqueue.size() > keep) {
+				// myqueue should only ever reach the size of keep
+				fileStr = myqueue.front();
+				remove( fileStr.c_str() ); //Takes char
+				myqueue.pop();
+
+				if (D_MPI_RANK == 0 ) {
+					restStr = myqueue_rst.front();
+					remove( restStr.c_str() );
+					myqueue_rst.pop();
+				}
+			}
 		}
 
 		return 0;
 	};
 
-int cbSaveCheckpoint::writeRestartFile() {
+int cbSaveCheckpoint::writeRestartFile( const char * fn, const char * rf ) {
 
 		// Check if a LoadBinary attribute exists
 		pugi::xml_node n1 = solver->configfile.child("CLBConfig").child("LoadBinary");
@@ -59,14 +81,14 @@ int cbSaveCheckpoint::writeRestartFile() {
 			// If it doesn't exist, create it before solve
 			n1 = solver->configfile.child("CLBConfig").child("Solve");
 			pugi::xml_node n2 = solver->configfile.child("CLBConfig").insert_child_before("LoadBinary", n1);
-			n2.append_attribute("file").set_value(fn.c_str());
+			n2.append_attribute("file").set_value(fn);
 		} else {
 			// If it does exist, remove it and replace it with up to date file string
 			n1.remove_attribute(n1.attribute("file"));
-			n1.append_attribute("file").set_value(fn.c_str());	
+			n1.append_attribute("file").set_value(fn);	
 		}
-		
-		solver->configfile.save_file(rf.c_str());
+
+		solver->configfile.save_file( rf );
 
 	
 	return 0;
--- src/Handlers/cbSaveCheckpoint.h
+++ src/Handlers/cbSaveCheckpoint.h
@@ -5,16 +5,17 @@
 
 #include "vHandler.h"
 #include "Callback.h"
+#include <queue>
 
 class  cbSaveCheckpoint  : public  Callback  {
-	std::string fn;
-	std::string rf;
-	bool overwrite;
+	int keep;
+	std::queue<std::string> myqueue;
+	std::queue<std::string> myqueue_rst;
 	public:
 		static std::string xmlname;
 		int Init ();
 		int DoIt ();
-		int writeRestartFile();
+		int writeRestartFile( const char * fn, const char * rf);
 };
 
 #endif // CBSAVECHECKPOINT_H
