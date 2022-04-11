d9af5311be612e6e9565e74e49e357e2cd2d3b2e
Dmytro Sashko <dmshko@gmail.com>
2022-04-11 18:34:42 +1000

    Add different geometric angle update methods

--- models/multiphase/d3q27_pf_velocity/conf.mk
+++ models/multiphase/d3q27_pf_velocity/conf.mk
@@ -1,6 +1,6 @@
 ADJOINT=0
 TEST=FALSE
-OPT="q27*ML*OutFlow*altContactAngle*BGK*thermo*planarBenchmark*autosym*staircaseimp*DEBUG"
+OPT="q27*ML*OutFlow*altContactAngle*BGK*thermo*planarBenchmark*autosym*staircaseimp*DEBUG*(gupdate1 + gupdate2 + gupdate3)"
 # q27 - Q27 lattice structure for phasefield
 # ML  - export densities for machine learning
 # OutFlow - include extra velocity stencil for outflowing boundaries
