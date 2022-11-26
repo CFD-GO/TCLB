a43a32b890066bbbdf834f85329b5427f55c79bd
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2022-11-26 21:55:17 +1000

    Adding position to lattice

--- src/Handlers/acGeometry.cpp
+++ src/Handlers/acGeometry.cpp
@@ -3,6 +3,16 @@ std::string acGeometry::xmlname = "Geometry";
 #include "../HandlerFactory.h"
 
 int acGeometry::Init () {
+		double px=0.0, py=0.0, pz=0.0;
+		bool write_pos = false;
+		pugi::xml_attribute attr;
+		attr = node.attribute("px");
+		if (attr) { px = solver->units.alt(attr.value()); write_pos = true; }
+		attr = node.attribute("py");
+		if (attr) { py = solver->units.alt(attr.value()); write_pos = true; }
+		attr = node.attribute("pz");
+		if (attr) { pz = solver->units.alt(attr.value()); write_pos = true; }
+		if (write_pos) solver->lattice->setPosition(px,py,pz);
 			if (solver->geometry->load(node)) {
 				error("Error while loading geometry\n");
 				return -1;
