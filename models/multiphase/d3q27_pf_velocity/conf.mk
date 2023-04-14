27edaae87bfff928bc828d9770754f2f3579385f
Dmytro Sashko <dmshko@gmail.com>
2023-04-14 17:37:51 +1000

    Explain different options

--- models/multiphase/d3q27_pf_velocity/conf.mk
+++ models/multiphase/d3q27_pf_velocity/conf.mk
@@ -8,5 +8,18 @@ OPT="(q27 + OutFlow  + BGK + thermo + planarBenchmark + autosym)*geometric*stair
 # BGK - single relaxation time operator
 # thermo - include energy equation solver for temperature field, influences through
 #        - the surface tension
+#
 # planarBenchmark - thermocapillary benchmark case
+#
 # autosym - symmetry boundary conditions
+#
+# geometric - use geometric boundary conditions instead of surface energy, sometimes gives more accurate results
+#
+# staircaseimp - use staircase improvement (applicable to both surface energy and geometric boundary conditions)
+#
+# isograd - use isotopic gradient also near boundaries when calculating phase field gradient, this
+# 	        essentially uses the value of the phase field gradient at the boundary from the previous iteration
+# 	        to calculate the phase field gradient at the boundary in the current iteration
+#
+# tprec - use more precise triangle for interpolation of phase field gradient (only applicable to geometric
+# 	   	  boundary conditions with staircase improvement)
