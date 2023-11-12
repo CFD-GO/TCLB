c31a36a35786cca847036ab203495313afd3f57e
L Laniewski-Wollk <uqllanie@uq.edu.au>
2023-11-13 09:51:25 +1000

    Adding RT tokenize to tools/format.sh

--- tools/format.sh
+++ tools/format.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 PP=$(dirname $0)
-clang-format --style=file:$PP/.clang-format "$@" \
+
+R -s -e 'rtemplate::RTtokenize()' \
+ | clang-format --style=file:$PP/.clang-format "$@" \
  | sed -E 's/for[[:blank:]]*[(]([[:alpha:]_]*[[:blank:]]|)[[:blank:]]*([[:alnum:]_]+)[[:blank:]]*=[[:blank:]]*([[:alnum:]_]+)[[:blank:]]*;[[:blank:]]*([[:alnum:]_]+)[[:blank:]]*([<=]*)[[:blank:]]*([[:alnum:]_]+)[[:blank:]]*;[[:blank:]]*([[:alnum:]_]+)[+][+][[:blank:]]*\)/for (\1\2=\3; \4\5\6; \7++)/g' \
- | sed -E 's| ([*/]) |\1|g'
+ | sed -E 's| ([*/]) |\1|g' \
+ | R -s -e 'rtemplate::RTtokenize(inv=TRUE)'
